80a4ca46fa4364fbaad32a25d181191edc3f51d7
Juzumaru <dochy.ksti@gmail.com>
2021-03-24 18:25:11 +0900

    あとはdefを呼ぶソースを生成するべし

--- dochy_intf/src/imp/structs/param_source.rs
+++ dochy_intf/src/imp/structs/param_source.rs
@@ -120,6 +120,36 @@ impl ParamSource{
         sb.push(0,"}");
         sb.to_string()
     }
+    pub fn get_def(&self, mod_name : &str) -> String{
+        let mut sb = SourceBuilder::new();
+
+        let id = self.name();
+        let snake_name = to_snake_name(id);
+        let is_old = self.is_old();
+        let var_type = self.var_type();
+        let pt = self.param_type();
+
+        sb.push(0,&format!("pub fn {}_def_val(&self) -> {}{{", with_old(&snake_name, is_old), with_var(&format!("&mut {}", pt.typename()), var_type)));
+        sb.push(1,&format!("let qv = {}::get_{}_def(self.ptr, \"{}\").unwrap();", mod_name, pt.nickname(), id));
+
+
+        match &var_type {
+            VarType::Normal => {
+                sb.push(1,&format!("qv.into_value().unwrap()"));
+            },
+            VarType::Undefiable => {
+                sb.push(1,&format!("UndefOr::from_qv(qv).unwrap()"));
+            },
+            VarType::Nullable => {
+                sb.push(1,&format!("NullOr::from_qv(qv).unwrap()"));
+            },
+            VarType::UndefNullable => {
+                sb.push(1,&format!("qv"));
+            },
+        }
+        sb.push(0,"}");
+        sb.to_string()
+    }
 
     pub fn set(&self, mod_name : &str) -> String {
         let mut sb = SourceBuilder::new();
