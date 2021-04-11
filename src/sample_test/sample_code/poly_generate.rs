fa6619797aa5edbb1a13def57b60cbdf0feb7d69
juzu <dochy.ksti@gmail.com>
2021-04-11 17:40:27 +0900

    snake_caseの変数名をIntfで型名にする時に、ちゃんと変換するようにした

--- src/sample_test/sample_code/poly_generate.rs
+++ src/sample_test/sample_code/poly_generate.rs
@@ -2,12 +2,12 @@ use dochy_core::json_dir_to_root;
 use dochy_intf::generate_interface;
 
 #[test]
-fn rpg1_generate() {
-    match json_dir_to_root("src/sample_test/sample_code_json/rpg1", true) {
+fn poly_generate() {
+    match json_dir_to_root("src/sample_test/sample_code_json/poly", true) {
         Ok(mut a) => {
             let ans = generate_interface(&mut a);
             std::fs::write(
-                "src/sample_test/sample_code/rpg1_accessor.rs",
+                "src/sample_test/sample_code/poly_accessor.rs",
                 &ans.to_string(),
             ).unwrap();
         }
