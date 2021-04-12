3bd83957dadadd19ee9ea12252a7ca13a867bda7
Juzumaru <dochy.ksti@gmail.com>
2021-04-12 17:07:12 +0900

    update準備

--- publish.bat
+++ publish.bat
@@ -1,24 +1,23 @@
 cd dochy_json5
 cargo publish
-timeout /t 10 > nul
+timeout /t 30 > nul
 cd ../dochy_compaction
 cargo publish
-timeout /t 10 > nul
+timeout /t 30 > nul
 cd ../dochy_core
 cargo publish
-timeout /t 10 > nul
+timeout /t 30 > nul
 cd ../dochy_archiver
 cargo publish
-timeout /t 10 > nul
+timeout /t 30 > nul
 cd ../dochy_diff
 cargo publish
-timeout /t 10 > nul
+timeout /t 30 > nul
 cd ../dochy_fs
 cargo publish
-timeout /t 10 > nul
+timeout /t 30 > nul
 cd ../dochy_intf
 cargo publish
-timeout /t 10 > nul
+timeout /t 30 > nul
 cd ../
 cargo publish
-timeout /t 10 > nul
