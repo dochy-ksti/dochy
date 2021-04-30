360114c282fb7d0c6906404676d12a6dde3b611c
juzu <dochy.ksti@gmail.com>
2021-04-30 15:54:00 +0900

    まだ

--- dochy_fs/src/imp/common/identity/ideneity_file.rs
+++ dochy_fs/src/imp/common/identity/ideneity_file.rs
@@ -1,7 +1,13 @@
 use dochy_core::structs::RustIdentity;
 use std::io::Write;
 use crate::history::FileNameProps;
+use crate::error::{FsResult};
+use dochy_compaction::kval_enum::KVal;
 
-pub(crate) fn write<W : Write>(write : &mut W, id : &RustIdentity, newest_file : &FileNameProps){
+pub(crate) fn write<W : Write>(write : &mut W, id : &RustIdentity, newest_file : &FileNameProps) -> FsResult<()>{
+    let mut vec : Vec<KVal> = vec![];
+    aa
 
+    dochy_compaction::encode(&vec, write)?;
+    Ok(())
 }
