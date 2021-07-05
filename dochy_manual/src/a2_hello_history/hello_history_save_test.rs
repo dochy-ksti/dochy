7c2ca6c92bf5c61385d716bd3550b225033d71c0
Juzumaru <dochy.ksti@gmail.com>
2021-07-05 16:17:49 +0900

    やっぱまだまだバグだらけぞ

--- dochy_manual/src/a2_hello_history/hello_history_save_test.rs
+++ dochy_manual/src/a2_hello_history/hello_history_save_test.rs
@@ -5,14 +5,15 @@ use dochy::fs::filesys::{save_file};
 use dochy::fs::common::{CurrentSrc, hash_dir_path};
 use std::path::{PathBuf, Path};
 use dochy::fs::history::{save_history_file, DochyCache, list_histories, load_history_file};
+use dochy::core::structs::RootObject;
 
 #[test]
 fn hello_history_save_test() -> DpResult<()> {
     let src_dir = "src/a2_hello_history/src_dir";
-    let root = json_dir_to_root(src_dir, true)?;
+    let mut root = json_dir_to_root(src_dir, true)?;
 
 
-    let history_dir = "src/a2_hello_history/history_dir";
+    let history_dir = Path::new("src/a2_hello_history/history_dir");
 
     std::fs::create_dir(history_dir).ok();
     let his = list_histories(history_dir, ())?;
@@ -20,41 +21,13 @@ fn hello_history_save_test() -> DpResult<()> {
     let mut cache = DochyCache::new(
         CurrentSrc::from_src_dir(src_dir));
 
-    let mut root = RootIntf::new(root);
-    root.set_data2("data2'".to_string());
-
-    save_history_file(history_dir, None, root.root_obj_ref(), &mut cache, ())?;
-
+    for counter in 0..10{
+        save_twice(history_dir, root, counter, &mut cache);
+        root = load_prev_file(history_dir, &mut cache)?;
+    }
     let his = list_histories(history_dir, ())?;
-    let file_data = his.get_newest_file_data().unwrap();
+    let d = his.get_newest_file_data()?;
 
-    let loaded1 = load_history_file(history_dir, file_data.hash(), file_data.props(), file_data.history(), &mut cache, (), false)?;
-    let mut loaded1 = RootIntf::new(loaded1);
-    assert_eq!(loaded1.data2(), "data2'".to_string());
-
-    root.set_data3("data3'".to_string());
-    save_history_file(history_dir, None, root.root_obj_ref(), &mut cache, ())?;
-
-    let his = list_histories(history_dir, ())?;
-    let file_data = his.get_newest_file_data().unwrap();
-
-    let loaded2 = load_history_file(history_dir, file_data.hash(), file_data.props(), file_data.history(), &mut cache, (), false)?;
-    let mut loaded2 = RootIntf::new(loaded2);
-    assert_eq!(loaded2.data2(), "data2'".to_string());
-    assert_eq!(loaded2.data3(), "data3'".to_string());
-
-    let his = list_histories(history_dir, ())?;
-    let d = his.list_files()[0];
-    let prev_load = load_history_file(history_dir, d.hash(), d.props(), d.history(), &mut cache, (), false)?;
-    let mut p_l = RootIntf::new(prev_load);
-    p_l.set_data3("data3'''".to_string());
-    let a = save_history_file(history_dir, None, p_l.root_obj_ref(), &mut cache, ())?;
-    let his = list_histories(history_dir, ())?;
-    let d = his.get_newest_file_data().unwrap();
-    let loaded3 = load_history_file(history_dir, d.hash(), d.props(), d.history(), &mut cache, (), false)?;
-    let loaded3 = RootIntf::new(loaded3);
-    assert_eq!(loaded3.data2(), "data2'".to_string());
-    assert_eq!(loaded3.data3(), "data3'''".to_string());
     print_file_data(hash_dir_path(history_dir, d.hash()))?;
     Ok(())
 }
@@ -65,4 +38,23 @@ fn print_file_data<P : AsRef<Path>>(path : P) -> DpResult<()>{
         println!("{} size {}", file.file_name().to_str()?, file.metadata()?.len());
     }
     Ok(())
+}
+
+fn save_twice(history_dir : &Path, root : RootObject, counter : usize, cache : &mut DochyCache) -> DpResult<()>{
+    let mut r = RootIntf::new(root);
+    let mut txt = r.data1();
+    txt.push_str(&format!("{}", counter));
+    r.set_data1(txt.clone());
+    save_history_file(history_dir, None, r.root_obj_ref(), cache, ())?;
+    txt.push_str(&format!("{}", counter+1));
+    r.set_data1(txt);
+    save_history_file(history_dir, None, r.root_obj_ref(), cache, ())?;
+    Ok(())
+}
+
+fn load_prev_file(history_dir : &Path, cache : &mut DochyCache) -> DpResult<RootObject>{
+    let his = list_histories(history_dir, ())?;
+    let v = his.list_files();
+    let d = v.get(v.len() - 2)?;
+    Ok(load_history_file(history_dir, d.hash(), d.props(), d.history(), cache, (), false)?)
 }
