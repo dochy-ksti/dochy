0c283f4e64064d2966299fc5922a54c8722b16c8
juzu <dochy.ksti@gmail.com>
2021-09-05 21:02:23 +0900

    manualをゴリゴリ書くぞ

--- dochy_manual/src/lib.rs
+++ dochy_manual/src/lib.rs
@@ -1,3 +1,7 @@
+//! Dochy's manual will be written in this crate.
+
+
+
 #[cfg(test)]
 #[allow(dead_code)]
 mod a1_hello_world;
@@ -7,5 +11,9 @@ mod a1_hello_world;
 #[cfg(test)]
 #[allow(dead_code)]
 mod a2_hello_history;
+
+#[cfg(test)]
+#[allow(dead_code)]
+mod a3_the_basics_of_dochy_lang;
 
 
