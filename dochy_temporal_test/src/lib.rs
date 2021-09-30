19fb174afb74134e05266d59b8b8f4b77f376ca1
Juzy <dochy.ksti@gmail.com>
2021-09-30 17:05:31 +0900

    ベンチをした。 ThreadPoolはある程度の1スレッドあたり作業量があればいくつ作ろうとほぼボトルネックにはならない BitVecはVec<u8>の3倍ぐらい読み取りが遅いようだ・・・？　別にいいじゃん Vec<u64>の方がちょっと早いという結果になったが・・・？

--- dochy_temporal_test/src/lib.rs
+++ dochy_temporal_test/src/lib.rs
@@ -1,11 +1,17 @@
-#[cfg(test)]
-#[allow(dead_code)]
-//mod mlist_mut;
-mod siyou;
+#![feature(test)]
+extern crate test;
+
+// #[cfg(test)]
+// #[allow(dead_code)]
+// mod siyou;
+//
+// #[cfg(test)]
+// #[allow(dead_code)]
+// mod temporal_test;
+// #[cfg(test)]
+// #[allow(dead_code)]
+// mod mlist_mut;
 
 #[cfg(test)]
-mod temporal_test;
-#[cfg(test)]
-#[allow(dead_code)]
-mod mlist_mut;
+mod benchan;
 
