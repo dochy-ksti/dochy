c8b81cf5adb3356b251eec5b5570cc3404e0a69e
juzu <dochy.ksti@gmail.com>
2021-10-03 15:56:58 +0900

    archiver2 実装完了かな？ テストもした

--- dochy_archiver2/src/testing/archive_test.rs
+++ dochy_archiver2/src/testing/archive_test.rs
@@ -2,6 +2,7 @@ use crate::{ArcResult, ArchiveOptions, read_archive_data_from_directory, Archive
 use std::lazy::{SyncLazy};
 use std::sync::Arc;
 use std::ops::Deref;
+use std::sync::mpsc::channel;
 
 pub(crate) static JSON_ARC_OPT : SyncLazy<ArchiveOptions> = SyncLazy::new(|| {
     ArchiveOptionsBuilder::new()
@@ -35,27 +36,3 @@ fn archive_test() -> ArcResult<()>{
     Ok(())
 }
 
-//最後にだけflushするようにしたところさらに少し縮まった
-
-//first_len 62
-//len 10 veclen() 62
-//len 7 veclen() 62
-//len 31 veclen() 62
-//len 5449 veclen() 62
-//veclen() 3028
-
-//毎回encoderをnewすると多少ファイルサイズが膨らむので、(そもそもencoderの初期化処理も入ってしまうし)
-//一気にやったほうがよろしいようだ
-//小さいデータは圧縮すると大きくなるが、やはりある程度大きければ縮むようだ
-
-//first_len 62
-//len 10 veclen() 90
-//len 7 veclen() 105
-//len 31 veclen() 144
-//len 5449 veclen() 3052
-
-//first vec len 62
-//len 10 vec.len 90
-//len 7 vec.len 115
-//len 31 vec.len 164
-//len 5449 vec.len 3082
