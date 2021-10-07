13bb9d5cc62872a54005bfea4767a5c89349eb65
Juzy <dochy.ksti@gmail.com>
2021-10-07 13:39:05 +0900

    dochy_archiver1を安全に削除できたようだ（前はなぜうまくいかなかったのか謎である・・・)

--- dochy_archiver/src/imp/mod.rs
+++
@@ -1,8 +0,0 @@
-pub(crate) mod create_archive_from_directory;
-pub(crate) mod structs;
-pub(crate) mod calc_hash;
-pub(crate) mod get_file_items_from_directory;
-pub(crate) mod read_archive_data;
-pub(crate) mod get_hash_and_metadata_from_dir;
-pub(crate) mod get_hash_and_metadata_from_archive;
-pub(crate) mod resume_archive;
--- dochy_archiver/src/imp/structs/mod.rs
+++
@@ -1,5 +0,0 @@
-pub(crate) mod file_item;
-pub(crate) mod metadata;
-pub(crate) mod metadata_item;
-pub(crate) mod archive_data;
-pub(crate) mod archive_options;
