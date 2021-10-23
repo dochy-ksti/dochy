45a6bff2401cc82dcf9a08671210f3429e77164a
juzu <dochy.ksti@gmail.com>
2021-10-23 17:52:00 +0900

    英語がつらい・・・つらい・・・
    save_history_file_testのmodifyを直したので全部結果が変わるからそれに応じて記述を修正すべし

--- dochy_manual/src/b2_save_history_files/load_history_file_test.rs
+++ dochy_manual/src/b2_save_history_files/load_history_file_test.rs
@@ -1,5 +1,5 @@
 use dochy::error::DpResult;
-use dochy::fs::history::{HistoryInfo, HistoryOptionsBuilder, CumulativeOptionsBuilder, load_history_file, list_histories, save_history_file};
+use dochy::fs::history::{HistoryInfo, HistoryOptionsBuilder, CumulativeOptionsBuilder, load_history_file, list_histories, save_history_file, FileHistories};
 use dochy::fs::common::{CurrentSrc, hash_dir_path};
 use crate::b2_save_history_files::save_history_files_accessor::RootIntf;
 use crate::b2_save_history_files::save_history_file_test::{modify, print_dir};
@@ -19,33 +19,29 @@ pub(crate) fn load_history_file_test() -> DpResult<()>{
     // The code above is copy-pasted from the previous section.
 
     // "FileHistories" is needed to load history files.
-    // FileHistories is the file list's list of "history_dir".
-    // history_dir has "hash_dir"s.
-    // The file list of hash_dir is FileHistory. FileHistories is a list of FileHistory.
     //
-    // hash_dir is the directory contains history files and a archive of Dochy Src.
-    // The hash is calculated from the Dochy Src, and the hash_dir is created with the directory named the hash code.
-    // Everytime Dochy Src is modified, a new archive file and a hash_dir is created,
-    // and history files created from the Dcohy Src is saved in the hash_dir.
-    let his = list_histories(&info)?;
-    // Gets the list of file data from it.
+    // history_dir contains hash_dirs like save_dir.
+    // FileHistory is the file list of the hash_dir,
+    // and FileHistories is the collection of FileHistory.
+    let his : FileHistories = list_histories(&info)?;
+    // Gets the list of files from it.
     let files = his.list_files();
     // Finds the file "_0_1_0.his".
     let file = files.iter().find(|file| file.props().calc_filename() == "_0_1_0.his")?;
     // Loads the file.
     let root = load_history_file(&info,
-                                 // FileNameProps is the propeties gotten from the filename.
+                                 // FileNameProps is the properties got from the filename.
                                  // It represents the dependencies and you can restore the filename precisely from it.
                                  file.props(),
-                                 // The file list of a hash_dir the file belongs to
+                                 // FileHistory is explained above.
                                  file.history(), false)?;
 
     let mut root = RootIntf::new(root);
     let mut count = 0;
 
     modify(&mut root, &mut count);
-    // When a history file is saved after a loading,
-    // it automatically depends on the loaded file.
+    // When a RootObject is loaded from a history file, and hasn't been saved before,
+    // the save file automatically depends on the loaded file.
     let _file = save_history_file(&info, None, root.root_obj_ref())?;
     // the hash_dir path can be calculated from history_dir and the hash code.
     let hash_dir = hash_dir_path(history_dir, info.hash());
@@ -54,16 +50,20 @@ pub(crate) fn load_history_file_test() -> DpResult<()>{
     // "(0)_1_1_0_0.his 64 bytes" is just created.
     // The first number "(0)" is the control number of the parent.
     // "(0)_1_1_0_0" means "Parent's Control 0 Control 1 Phase-0 1 Phase-1 0 Phase-2 0"
-    // Parent files can be always identified from child file's filenames.
-    // Child file's phase numbers except the last is always identical with the parent's.
-    // The parent's control is 0, so the parent's number is "Control 0 Phase-0 1 Phase-1 0".
+    // The parent of a file can be identified from the filename.
+    // The child file's phase numbers except the last is always identical with the parent's, so "Phase-0 1 Phase-1 0".
+    // The parent's control is 0, so the parent's numbers are "Control 0 Phase-0 1 Phase-1 0".
     // The filename was "_0_1_0.his", so they are matched.
 
-    // We can't identify the grandparent because we can't know the grandparent's control number from a filename,
-    // so we need entire FileHistory to enumerate its ancestors
+    // We can't get the parent's tag from the filename, so we can't completely restore the filename of the parent.
+    // We need FileHistory to get the filename from the phase numbers and the control number.
 
-    // When history files are derived from files which is not the latest,
-    // or created without their parents, new control numbers are attached to their new filenames.
+    // We can't identify the grandparent because we can't get the grandparent's control number from the filename.
+    // We need the parent's filename to identify the grandparent.
+    // We can enumerate the ancestors of a file by finding the parent repeatedly.
+
+    // When the parent file is not the latest,
+    // or a file is created without a parent, a new control number will be provided to the filename.
 
     Ok(())
 }
